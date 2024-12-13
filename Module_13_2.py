ee58749000e57e7e5d00a83d1e54cd5a0d1935f2
Demyan k. <demiandiaz.dd@gmail.com>
2024-12-14 02:28:56 +0300

    Update Module_13_2.py

--- Module_13_2.py
+++ Module_13_2.py
@@ -15,13 +15,11 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     print('Привиет! Я бот помогающий твоему здоровью.')
-    await message.answer('Привиет! Я бот помогающий твоему здоровью.')
 
 
 @dp.message_handler(lambda message: True)
 async def all_messages(message: types.Message):
     print('Введите команду /start, чтобы начать общение.')
-    await message.answer('Введите команду /start, чтобы начать общение.')
 
 
 if __name__ == '__main__':
